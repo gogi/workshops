627450b724c5f3a2db045138e684f0de7f63e99a
Małgorzata Grobelska <grobelska.m@gmail.com>
2015-05-11 15:56:36 +0200

    Cosmetic change on rubocop's demand

--- app/decorators/review_decorator.rb
+++ app/decorators/review_decorator.rb
@@ -1,9 +1,8 @@
 class ReviewDecorator < Draper::Decorator
   delegate_all
 
-  def author  
+  def author
     user = review.user
-    user.firstname.to_s + " " + user.lastname.to_s
+    user.firstname.to_s + ' ' + user.lastname.to_s
   end
-
 end
